16fcddfbd5525fae59cc6d2d000968634cac3bc9
yuduopan6-web <yuduopan6@gmail.com>
2026-04-20 20:37:01 +0900

    Update app.py

--- fracture-ai/app.py
+++ fracture-ai/app.py
@@ -31,4 +31,6 @@ def predict():
     return jsonify({"result": CLASSES[pred]})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
